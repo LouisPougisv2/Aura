46e8222b07a666a015f455e9117278e0861b3ea6
Louis <louis.pougis@gmail.com>
2023-10-07 10:43:53 +0200

    1st try with an EffectActor in the world modifying attribute
    Notes: The code will be refactored with the PROPER way to modify attribute. It was mainly to learn how to access the AttributeSet

---
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,48 @@
+// Copyright Louis Pougis, All Rights Reserved.
+
+
+#include "Actor/AuraEffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+AAuraEffectActor::AAuraEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	SetRootComponent(StaticMesh);
+	
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SphereComponent->SetupAttachment(GetRootComponent());
+}
+
+void AAuraEffectActor::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//TODO : To change this to apply a Gameplay Effect (need to learn how). For now using const cast, gross...
+	//TODO : This will be refactored ASAP
+	if(IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+		UAuraAttributeSet* MutableAuraAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributeSet);
+		MutableAuraAttributeSet->SetHealth(AuraAttributeSet->GetHealth() + 20.0f);
+		Destroy();
+	}
+}
+
+void AAuraEffectActor::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	
+}
+
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SphereComponent->OnComponentBeginOverlap.AddUniqueDynamic(this, &AAuraEffectActor::OnOverlapBegin);
+	SphereComponent->OnComponentEndOverlap.AddUniqueDynamic(this, &AAuraEffectActor::OnOverlapEnd);
+}
+
+
