16adb43eede1c340540ea8eff98d3eb9cd495eb6
Louis <louis.pougis@gmail.com>
2023-10-10 15:30:30 +0200

    Globe Progress Bar

---
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -0,0 +1,31 @@
+// Copyright Louis Pougis, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "AuraWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraWidgetController : public UObject
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<APlayerController> PlayerController;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<APlayerState> PlayerState;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<class UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<class UAttributeSet> AttributeSet;
+	
+};
---
+++ Source/Aura/Public/UI/Widgets/AuraUserWidget.h
@@ -0,0 +1,29 @@
+// Copyright Louis Pougis, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuraUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UObject> WidgetController;
+
+	UFUNCTION(BlueprintCallable)
+	void SetWidgetController(UObject* InWidgetController);
+protected:
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnWidgetControllerSet();
+	
+};
