ef3fdd0f130ebefabb42d884802604ce9daf87fe
Louis <louis.pougis@gmail.com>
2024-01-11 15:54:29 +0100

    Armor & Armor Penetration Implemented

--- Source/Aura/Private/AbilitySystem/ExecutionCalculation/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecutionCalculation/ExecCalc_Damage.cpp
@@ -8,6 +8,7 @@
 UExecCalc_Damage::UExecCalc_Damage()
 {
 	RelevantAttributesToCapture.Add(GetDamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(GetDamageStatics().ArmorPenetrationDef);
 	RelevantAttributesToCapture.Add(GetDamageStatics().BlockingChanceDef);
 }
 
@@ -30,17 +31,32 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	//Get Damage Set by Caller
 	float Damage = Spec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
 
-	//Capture Block chance on Target & if there was a successful block
-	// If block, Halve the damage.
+	//Capture Block chance on Target
 	float TargetBlockChance = 0.0f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetDamageStatics().BlockingChanceDef, EvaluateParameters, TargetBlockChance);
-
 	TargetBlockChance = FMath::Max<float>(TargetBlockChance, 0.0f);
 
+	// If successful block, Halve the damage.
 	if(FMath::RandRange(1, 100) < TargetBlockChance)
 	{
 		Damage /= 2.0f;
 	}
+
+	//Target Armor Capture
+	float TargetArmor = 0.0f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetDamageStatics().ArmorDef, EvaluateParameters, TargetArmor);
+	TargetArmor = FMath::Max<float>(TargetArmor, 0.0f);
+
+	//Target Penetration Capture
+	float SourceArmorPenetration = 0.0f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetDamageStatics().ArmorPenetrationDef, EvaluateParameters, SourceArmorPenetration);
+	SourceArmorPenetration = FMath::Max<float>(SourceArmorPenetration, 0.0f);
+
+	//Armor Penetration ignores a percentage of the Target's Armor
+	//SourceArmorPenetration * 0.25 is used so it would take 400 armor to ignore 100%. It will take 4 Armor Penetration point to ignore 1% of Armor
+	const float EffectiveArmor = TargetArmor *= (100.0f - SourceArmorPenetration * 0.25f) / 100.0f;
+	Damage *= (100.0f - EffectiveArmor) / 100.0f;	//If too OP, we can to nerf if by multiplying EffectiveArmor * .3 (for example)
+	
 	const FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
