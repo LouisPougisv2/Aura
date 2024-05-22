d8e3c6e4e9c53f28de318cf15ee7d51150180a4f
Louis <louis.pougis@gmail.com>
2024-05-22 21:28:49 +0200

    Level Up Data Asset created

---
+++ Source/Aura/Public/AbilitySystem/Datas/LevelUpInfo.h
@@ -0,0 +1,38 @@
+// Copyright Louis Pougis, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FAuraLevelUpInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 LevelUpRequirement = 0;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 AttributePointsReward = 1;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 SpellPointReward = 1;
+};
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FAuraLevelUpInfo> LevelUpInformations;
+
+	int32 FindLevelForExp(int32 Exp);
+};
